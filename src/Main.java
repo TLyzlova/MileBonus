baf831fc24bc8c73959be89663bdeac51aa0ab9a
Lyzlova Tatiana <yatanita@gmail.com>
2021-04-16 20:39:05 +0300

    New files added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args){
+        int priceTicket = 200;
+        int sum = 20;
+        int miles = priceTicket / sum;
+        System.out.println(miles);
+    }
+}
